a69960cd8f3133883de0656b280c93b3219fcb3c
jupemartin <juanpedro.martin@sdggroup.com>
2024-06-18 13:10:03 +0200

    Create deployer.sh

---
+++ deployer.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+# Parameters
+STACK_NAME=$1
+TEMPLATE_FILE=$2
+ENVIRONMENT=$3
+
+# Build SAM application
+sam build --use-container --template-file ${TEMPLATE_FILE}
+
+# Deploy SAM application
+sam deploy --template-file .aws-sam/build/template.yaml \
+            --stack-name ${STACK_NAME} \
+            --capabilities CAPABILITY_IAM \
+            --parameter-overrides Environment=${ENVIRONMENT}
