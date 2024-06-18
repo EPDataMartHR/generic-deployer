a3dc6c7fa690d726b4d366597f00792671820bcc
jupemartin <juanpedro.martin@sdggroup.com>
2024-06-18 16:28:21 +0200

    Update deployer.sh

--- deployer.sh
+++ deployer.sh
@@ -11,12 +11,9 @@ ENVIRONMENT=$3
 # Build SAM application
 sam build --use-container --template-file ${TEMPLATE_FILE}
 
-echo $STACK_NAME
-echo $ENVIRONMENT
-
 # Deploy SAM application
 sam deploy --template-file .aws-sam/build/template.yaml \
-            --stack-name ${STACK_NAME} \
+            --stack-name "${STACK_NAME}" \
             --capabilities CAPABILITY_IAM \
-            --parameter-overrides Environment=${ENVIRONMENT}
-            --region $AWS_REGION
+            --parameter-overrides "Environment=${ENVIRONMENT}"
+            --region "$AWS_REGION"
